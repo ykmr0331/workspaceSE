230ee668c66b37d109c0374a3713d5dadf4112af
ITWILL <ykmr0331@naver.com>
2023-06-02 18:18:25 +0900

    6월2일 마지막 커밋

--- 99_JAVA_PRACTICE/src/com/itwill04/array/car/CarArrayMain.java
+++ 99_JAVA_PRACTICE/src/com/itwill04/array/car/CarArrayMain.java
@@ -22,11 +22,21 @@ public class CarArrayMain {
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ			
 		System.out.println("1.전체 차량출력");
 		Car.headerPrint();
+		/*
 		for (int i = 0; i < carArray.length; i++) {
 			if(carArray[i] != null) {
 			carArray[i].print();
 			}
 		}
+		*/
+		
+	 	for ( Car car : carArray) {
+			if(car != null) {
+				car.print();
+			}
+		}
+		
+		
 		System.out.println();
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ	
 		System.out.println("2.전체주차구획수");
@@ -36,11 +46,20 @@ public class CarArrayMain {
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ			
 		System.out.println("3.남은주차구획수");
 		int restSpace = 0;
+		/*
 		for (int i = 0; i < carArray.length; i++) {
 			if(carArray[i] != null) {
 				restSpace += 1;
 			}
 		}
+		*/
+		
+		for ( Car car : carArray) {
+			if(car != null) {
+				restSpace += 1;
+			}
+		}
+		
 		System.out.println("남은 주차구획수 >>" +(carArray.length- restSpace)+"자리");
 		System.out.println();
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ			
@@ -50,40 +69,80 @@ public class CarArrayMain {
 		 * 2.빈자리찾아서대입
 		 */
 		Car car1 = new Car("3534",10);
+		/*
 		for (int i = 0; i < carArray.length; i++) {
 			if(carArray[i] == null) {
 				carArray[i] = car1;
 
 				break;
 			}		
-		}	
+		}
+		*/	
+		
+		for (Car car : carArray) {
+			if(car == null) {
+				car= car1;
+
+				break;
+			}
+		}
+		
 		System.out.println();
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ			
 		System.out.println("5.차량번호 7789번  차한대 정보출력");
 		Car.headerPrint();
+		/*
 		for (int i = 0; i < carArray.length; i++) {
 			if((carArray[i] != null) &&((carArray[i].getNo()).equals("7789"))) {
 				carArray[i].print();
 				break;
 			}
 		}
+		*/
+		
+	
+		for (Car car : carArray) {
+			if(car != null) {
+				if(car.getNo().equals("7789")) {
+					car.print();
+					break;
+				}
+			}
+			
+		}
+	 
 		System.out.println();
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ		
 	
 		System.out.println("6.입차시간 10시이후 차량여러대 찾아서 정보출력");
+		
 		Car.headerPrint();
+		/*
 		for (int i = 0; i < carArray.length; i++) {	
-			if((carArray[i] != null) && (carArray[i].getInTime() >= 10)) {
+			if((carArray[i] != null) && (carArray[i].getInTime() > 10)) {
 				carArray[i].print();
 				
 			}
 		}
+		*/
+		
+		
+		for ( Car car : carArray) {
+			if(car != null) {
+				if(car.getInTime() >10) {
+					car.print();
+				}
+			}
+		}
+		
 		System.out.println();
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ			
 		System.out.println("7.2389번차량 12시 출차");
+		
 		Car.headerPrint();
+		/*
 		for (int i = 0; i < carArray.length; i++) {
-			if((carArray[i] != null) ||(carArray[i].getNo()).equals("2389")) {
+			if((carArray[i] != null) &&(carArray[i].getNo()).equals("2389")) {
 				carArray[i].setOutTime(12);
 				carArray[i].calculateFee();
 				carArray[i].print();
@@ -91,6 +150,19 @@ public class CarArrayMain {
 				break;
 			}
 		}
+		*/
+		
+		
+		for ( Car car : carArray) {
+			if((car!= null) &&(car.getNo()).equals("2389")) {
+				car.setOutTime(12);
+				car.calculateFee();
+				car.print();
+				car =null;
+				break;
+			}
+		}
+ 		
 
 		System.out.println();
 		/*
@@ -103,26 +175,25 @@ public class CarArrayMain {
 		
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ		
 		System.out.println("99. 2389번차량 출차후전체 차량출력");
+		
 		Car.headerPrint();
+		/*
 		for (int i = 0; i < carArray.length; i++) {
 			if(carArray[i] != null) {
 			carArray[i].print();
 			}
 		}
+		*/
 		
 		
-		//Car.headerPrint();
-		//for (int i = 0; i < carArray.length; i++) {
-		//	if(carArray[i] != null) {
-		//	carArray[i].print();
-		
+		for ( Car car : carArray) {
+			if(car != null) {
+				car.print();
 			}
+		}
 		
-		
-		
-		
-	
 
+	}
 }
 
 
