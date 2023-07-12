8e572640d86c60d247828fba28ba0ca06ad7d459
ITWILL <ykmr0331@naver.com>
2023-07-12 20:47:05 +0900

    230712

--- 25.JDBC[Java DataBase Connectivity]/src/dao/member2/MemberDaoTestMain.java
+++ 25.JDBC[Java DataBase Connectivity]/src/dao/member2/MemberDaoTestMain.java
@@ -5,7 +5,7 @@ public class MemberDaoTestMain {
 	public static void main(String[] args) throws Exception {
 		MemberDao memberDao=new MemberDao();
 		
-		Member insertTest = new Member("서울대7", "로스쿨", "아닙니다ㅋ", "낙성대학교", 24, 'M', null);
+		Member insertTest = new Member("서울대6", "로스쿨", "아닙니다ㅋ", "낙성대학교", 24, 'M', null);
 		Member updateTest = new Member("서울대7", "하이스쿨", "맞습니다", "신림동", 34, 'F', null);
 		
 		System.out.println("0.delete   --> " + memberDao.delete("서울대4"));
