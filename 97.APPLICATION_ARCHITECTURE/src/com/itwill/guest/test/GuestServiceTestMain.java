f7cc7e106e55eea11c94d666a30c2502ec33c4e9
ITWILL <ykmr0331@naver.com>
2023-07-14 09:35:26 +0900

    230714

--- 97.APPLICATION_ARCHITECTURE/src/com/itwill/guest/test/GuestServiceTestMain.java
+++ 97.APPLICATION_ARCHITECTURE/src/com/itwill/guest/test/GuestServiceTestMain.java
@@ -9,17 +9,27 @@ public class GuestServiceTestMain {
 		GuestService guestService=new GuestService();
 		System.out.println("1.방명록쓰기");
 		System.out.println(guestService.guestWrite(new Guest(0, "아무개", null, "uunknown@naver.com", "www.google.com", "몰라요", "내용도몰라요")));
+	
+		
 		System.out.println("4.방명록1개보기");
-		Guest guest=guestService.guestDetail(24);
+		Guest guest=guestService.guestDetail(4);
 		System.out.println(">>"+guest);
+		
+		
 		System.out.println("2.방명록수정");
 		guest.setGuest_name("수정이름");
 		guest.setGuest_homepage("www.change.co.kr");
 		System.out.println(">>"+ guestService.guestUpdate(guest));
+	
+		
 		System.out.println("3.방명록삭제");
-		System.out.println(">> "+guestService.guestDelete(10));
+		System.out.println(">> "+guestService.guestDelete(7));
+	
+		
 		System.out.println("5.방명록이름으로검색 보기");
 		System.out.println(">> "+guestService.findByGuestName("김봉선"));
+	
+		
 		System.out.println("6.방명록 리스트보기");
 		System.out.println(">> "+guestService.guestList());
 	}
